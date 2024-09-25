12a399495731468c4660e7aeed1f229ffb9e1b1e
ianmarinovski <144111969+DegenYan@users.noreply.github.com>
2024-09-25 18:53:49 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import WebApp from "@twa-dev/sdk";
+import { useEffect, useState } from "react";
+
+// Define the interface for user data
+interface UserData {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code: string;
+  is_premium?: boolean;
+}
+
+export default function Home() {
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    console.log(WebApp.initDataUnsafe.user);
+    if (WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user as UserData);
+    }
+  }, []);
+
+  return (
+    <main className="p-4">
+      {userData ? (
+        <>
+          <h1 className="text-2xl font-bold mb-4">User Data</h1>
+          <ul>
+            <li>
+              ID:{" "}
+              {userData.id
+                .toString()
+                .split("")
+                .map((a) => "*")}
+            </li>
+            <li>First Name: {userData.first_name}</li>
+            <li>Last Name: {userData.last_name || "N/A"}</li>
+            <li>Username: {userData.username || "N/A"}</li>
+            <li>Language Code: {userData.language_code}</li>
+            <li>Is Premium: {userData.is_premium ? "Yes" : "No"}</li>
+          </ul>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </main>
+  );
+}
